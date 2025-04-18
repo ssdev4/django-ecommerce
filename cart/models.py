67e4133bf381579919fc31c1167ff991a84c99b0
Umer Naseem <umardev95@gmail.com>
2025-04-18 15:05:26 -0700

    Updated Cart Detial

--- cart/models.py
+++ cart/models.py
@@ -10,6 +10,10 @@ class Cart(models.Model):
     def __str__(self):
         return f"Cart ({self.user or self.session_key})"
 
+    @property
+    def total(self):
+        return sum(item.subtotal for item in self.items.all())
+
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -17,3 +21,7 @@ class CartItem(models.Model):
 
     def __str__(self):
         return f"{self.quantity} x {self.product.name}"
+
+    @property
+    def subtotal(self):
+        return self.product.price * self.quantity
