37b0fb9e32d682bbdcd79990539220addbbc7834
Umer Naseem <umardev95@gmail.com>
2025-04-18 14:41:05 -0700

    Added Product detail page

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import ProductListView
+from .views import ProductListView, ProductDetailView
+
+app_name = 'products' 
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
+    path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Product
 
 class ProductListView(ListView):
     model = Product
     template_name = 'products/home.html'
     context_object_name = 'products'
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
