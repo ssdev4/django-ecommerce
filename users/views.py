de46846863d13ecb42a77c6aac416ca400bde799
Umer Naseem <umardev95@gmail.com>
2025-04-20 13:41:54 -0700

    Allow guests to convert their guest order into a real account on signup.

--- users/views.py
+++ users/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
 from django.contrib.auth import login
+from checkout.models import Order
 
 def signup(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
+            # import pdb; pdb.set_trace()
+            session_key = request.session.session_key
             user = form.save()
             login(request, user)  # Automatically log in user after signup
-            return redirect('/')  # or wherever you want to redirect
+            # Transfer any guest orders to the new user
+            if session_key:
+                Order.objects.filter(user__isnull=True, session_key=session_key).update(user=user)
+
+            return redirect('/')
     else:
         form = CustomUserCreationForm()
     return render(request, 'users/signup.html', {'form': form})
